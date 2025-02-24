5f94cfc861589e0804ad9ac93c45a938e59d1aeb
Hak9997 <anonenforcer777@gmail.com>
2025-02-24 16:38:28 +0000

    update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,1 +1,13 @@
+#/bin/bash
 
+# Try to install nmap with sudo
+if sudo ping -c 1 google.com; then
+    apt install root-repo -y
+    apt install hping 3 -y 
+    apt install macchanger -y
+    apt install nmap -y
+    echo "root installation succeeded with sudo."
+else
+    sleep 4
+    echo "nmap installation failed with sudo. Running apt update without sudo and retrying..."
+fi
