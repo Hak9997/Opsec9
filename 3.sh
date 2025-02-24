14eea2eacffa69cc1b6c928c3208b1d091dfe440
Hak9997 <anonenforcer777@gmail.com>
2025-02-24 19:10:21 +0000

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+echo -e "\033[31m╔═════════════════════════════════════════════╗\033[0m"
 echo -e "\033[5m\033[31mTesting to see if you are root  \033[0m"
 # Try to install nmap with sudo
 if sudo ping -c 1 google.com; then
