76a25dae98d5f23e787f2538cf0d6488321286e8
Hak9997 <anonenforcer777@gmail.com>
2025-02-24 19:00:34 +0000

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -45,7 +45,7 @@ pkg installl proot -y
 pkg install termux-api -y
 pkg install tsu
 # Run the other script
-source /data/data.com.termux/file/home/Opsec9/3.sh&
+source /data/data.com.termux/file/home/Opsec9/3.sh &
 pkg install macchanger -y&
 sleep 5
 # Update the package list
