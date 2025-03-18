a06d222a4e8f4f33ca3e5b3a39591e66c4108eaa
Hak9997 <anonenforcer777@gmail.com>
2025-03-18 20:11:29 +0000

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -3,13 +3,17 @@ echo -e "\033[31m╔════════════════════
 echo -e "\033[5m\033[31mTesting to see if you are root  \033[0m"
 echo -e "\033[31m╚═════════════════════════════════════════════╝\033[0m"
 # Try to install nmap with sudo
-if sudo ping -c 1 google.com; then
-    apt install root-repo -y
+#!/bin/bash
+
+# Attempt to run the ping command
+if sudo ping -c 1 www.google.com >/dev/null 2>&1; then
+    echo "Ping command executed successfully."
+     apt install root-repo -y
     apt install hping 3 -y 
     pkg install macchanger -y
-    apt install nmap -y
-termux-toast -b red -c blue -g top "Root is installed on this device"
+   apt install nmap -y
+    termux-toast -b red -c blue -g top "Root is installed on this device"
 else
-termux-toast -b red -c blue -g top "Root is not installed on this device"
-    sleep 1
+    # Show a toast message in Termux
+    termux-toast "Root is not installed on this device"
 fi
