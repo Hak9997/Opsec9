a8c40b4e4d03529e5c1322affae1bed6a0bb3b72
Hak9997 <anonenforcer777@gmail.com>
2025-03-19 16:45:01 +0000

    Update wns.sh

--- wns.sh
+++ wns.sh
@@ -4,13 +4,13 @@
 # Check for iwlist availability
 if command -v iwlist &> /dev/null; then
     echo "Scanning Wi-Fi networks using iwlist..."
-    echo "------------------------------------------"
+   echo -e "\033[31m─────────────────────────────────────────────────────────────\033[0m"
     iwlist wlan0 scan | grep -E "ESSID|Signal|Channel" | awk -F: '
         /ESSID/ {print "SSID: " substr($2, 2, length($2)-2)}
         /Signal/ {print "Signal: " $2}
         /Channel/ {print "Channel: " $2 "\n"}
     '
-    echo "------------------------------------------"
+   echo -e "\033[31m─────────────────────────────────────────────────────────────\033[0m"
 else
     echo "iwlist is not installed. Please install it or use another tool!"
 fi
