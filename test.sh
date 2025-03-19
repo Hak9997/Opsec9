52a3e8798b3e100fa119fbad84f485258121cbfe
Hak9997 <anonenforcer777@gmail.com>
2025-03-19 15:43:21 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,26 +1,72 @@
 #!/bin/bash
 
+ 
 as=$(lscpu | grep 'Model name' | awk -F ':' '{print $2}')
 
+echo -e "\033[31m╔═══════════════════════════════════════════════════════════════════════════════════════════════════╗\033[0m"
+echo -e "\033[31m███╗   ██╗███████╗████████╗██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗    ███████╗ ██████╗ █████╗ ███╗   ██╗\033[0m"
+echo -e "\033[31m████╗  ██║██╔════╝╚══██╔══╝██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝    ██╔════╝██╔════╝██╔══██╗████╗  ██║\033[0m"
+echo -e "\033[31m██╔██╗ ██║█████╗     ██║   ██║ █╗ ██║██║   ██║██████╔╝█████╔╝     ███████╗██║     ███████║██╔██╗ ██║\033[0m"
+echo -e "\033[31m██║╚██╗██║██╔══╝     ██║   ██║███╗██║██║   ██║██╔══██╗██╔═██╗     ╚════██║██║     ██╔══██║██║╚██╗██║\033[0m"
+echo -e "\033[31m██║ ╚████║███████╗   ██║   ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗    ███████║╚██████╗██║  ██║██║ ╚████║\033[0m"
+echo -e "\033[31m╚═╝  ╚═══╝╚══════╝   ╚═╝    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝\033[0m"
+echo -e "\033[31m╚═══════════════════════════════════════════════════════════════════════════════════════════════════╝\033[0m"
+function yes_or_no {
+  while true; do
+    read -p "$* [y/n/*]: " yn
+    case $yn in
+      [Yy]* ) return 0;;
+      [Nn]* ) return 1;;
+      * )
+      clear
+      echo -e "\033[5m\033[31mThats not a option \033[0m";;
+    esac
+  done
+}
 
-# Retrieve the default network interface
+while getopts 'v' OPTION; do
+  case "$OPTION" in
+    v)
+    echo -e "---------------------------------------"
+    echo "Version 1.0."
+    echo "---------------------------------------"
+    echo -e "\033[5m\033[31m Opsec 9 tools\033[0m"
+    echo "---------------------------------------"
+     exit
+    ;;
+    ?) echo "Usage: $(basename $0) [-v]" >&2; exit 1;;
+  esac
+done
+
+if yes_or_no "Do you want to scan the network"; then
+# Get the IP address of the default interface
 interface=$(ip route | grep default | awk '{print $5}')
-if [ -z "$interface" ]; then
-    echo "Error: Failed to determine the network interface. Check your network configuration."
-    exit 1
-fi
-
-echo "Network Interface: $interface"
-
-# Retrieve the IP address for the interface
 ip_address=$(ip addr show $interface | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-if [ -z "$ip_address" ]; then
-    echo "Error: Failed to determine the IP address. Ensure the interface has a valid IP address."
-    exit 1
+# Scan the network for all devices
+echo -e "\033[31m╔═════════════════════════════════════════════╗\033[0m"
+echo -e "██╗██████╗ ███████╗"
+echo -e "██║██╔══██╗██╔════╝"
+echo -e "██║██████╔╝███████╗"
+echo -e "██║██╔═══╝ ╚════██║"
+echo -e "██║██║     ███████║"
+echo -e "╚═╝╚═╝     ╚══════╝"
+                   
+echo -e "\033[31m╚═════════════════════════════════════════════╝\033[0m"
+echo -e "\033[30m─────────────────────────────────────────────────────────────\033[0m "
+echo -e "\033[31m╔═══════════════════════════════════════════════════════════════╗\033[0m"
+nmap -sn -oX scan_results.xml $ip_address/24
+wait
+sudo nmap -sP -O --script=mac-address scan_results.xml
+echo -e "\033[31m╚═══════════════════════════════════════════════════════════════╝\033[0m"
+else
+  exit
 fi
 
-echo "IP Address: $ip_address"
 
-# Perform an nmap scan on the subnet
-echo "Scanning the subnet $ip_address/24..."
-sudo nmap -sn "$ip_address/24" -oG - | awk '/Host:/{print $2, $3}' | column -t -N "IP_Address,Status"
+
+
+
+
+
+
+
