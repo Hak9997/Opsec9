2604137c88e14e694be4ef620bac142a5140a126
Hak9997 <anonenforcer777@gmail.com>
2025-03-20 23:19:47 +0000

    Update tail.sh

--- tail.sh
+++ tail.sh
@@ -1,1 +1,5 @@
+tail -f port_scan_log.txt | while read line; do
+    SRC_IP=$(echo "$line" | grep -oP '(?<=IP )[^ ]+')
+    termux-notification -t "Port Scan Detected" -c "Suspicious activity from $SRC_IP"
+done
 
