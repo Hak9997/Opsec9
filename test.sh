20f772c8d7b0b1dacf6797cc32ad55ac2d0904d6
Hak9997 <anonenforcer777@gmail.com>
2025-03-20 23:17:50 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,2 @@
-#!/bin/bash
 
-INTERFACE="wlan0"
-
-tcpdump -i $INTERFACE 'tcp[tcpflags] & (tcp-syn) != 0' -l | while read line; do
-    # Log the detected packet
-    echo "$line" >> port_scan_log.txt
-
-    # Extract source IP
-    SRC_IP=$(echo "$line" | grep -oP '(?<=IP )[^ ]+')
-    cd /data/data/com.termux/files/usr/bin/
-    # Send notification and log errors
-    /data/data/com.termux/files/usr/bin/termux-notification -t "Port Scan Detected" -c "Suspicious activity from $SRC_IP" 2>>notification_error_log.txt
-done
+sudo tcpdump -i wlan0 'tcp[tcpflags] & (tcp-syn) != 0' -l >> port_scan_log.txt &
