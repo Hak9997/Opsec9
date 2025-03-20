cdc90a054ff212b8066ede664f6eeb38d324fd38
Hak9997 <anonenforcer777@gmail.com>
2025-03-20 22:11:33 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ tcpdump -i $INTERFACE 'tcp[tcpflags] & (tcp-syn) != 0' -l | while read line; do
 
     # Extract source IP
     SRC_IP=$(echo "$line" | grep -oP '(?<=IP )[^ ]+')
-
+    cd /data/data/com.termux/files/usr/bin/
     # Send notification and log errors
     /data/data/com.termux/files/usr/bin/termux-notification -t "Port Scan Detected" -c "Suspicious activity from $SRC_IP" 2>>notification_error_log.txt
 done
